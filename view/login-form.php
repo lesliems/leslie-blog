65bdab06c9ef70cd9e12a4e2a56745391ff152ea
leslie Segovia <lesliemsegovia@gmail.com>
2015-02-26 00:04:36 -0800

    hgc

--- view/login-form.php
+++ view/login-form.php
@@ -2,24 +2,38 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");
 ?>
-<body>
-<h1>Login</h1>
+<body class="loginb">
+    <header>
+    <video autoplay loop muted class="bgvideo" id="vid">
+        <source src="video/happy.mp4" type="video/mp4">
+    </video>
+        <nav class="navbar navbar-inverse" id="navb">
+             <a href='index.html'>Home</a>
+                <a href='projects.html'>Projects</a>
+                <a href='technology.html'>Technology</a>
+                <a href='more.html'>More</a>
+                <a href='other.html'>Other</a>
+                <a href='login.php'>Login</a>
+                <a href='login.php'>Logout</a>            
+        </nav>
+<h1 class="login">Login</h1>
 
 <form method="post"action="<?php echo $path . "controller/login-user.php"; ?>">  
  <div>
-     <label for="username" Username: </label>
+     <label for="username" class="user">Username: </label>
         <input type="text" name="username"/>
     </div>
     
     <div>
-        <label for="password" >Password: </label>
+        <label for="password" class="pass">Password: </label>
         <input type="password" name="password"/>
     </div>
         
         <div>
-            <button type="submit">Submit</button>
+            <button type="submit">Login</button>
         </div>
 
     
 </form>
+    </header>
 </body>
